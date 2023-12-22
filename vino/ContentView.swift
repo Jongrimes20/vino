48941e323d2ff3843f93c41a334e2cd5f8352bc4
Jon Grimes <JonGrimes20@gmail.com>
2023-12-22 12:28:17 -0600

    Initial Commit

---
+++ vino/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  vino
+//
+//  Created by Jon Grimes on 12/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ vino/vinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  vinoApp.swift
+//  vino
+//
+//  Created by Jon Grimes on 12/22/23.
+//
+
+import SwiftUI
+
+@main
+struct vinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
